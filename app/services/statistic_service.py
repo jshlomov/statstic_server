11ad74c52bc13b13fa77917c93a0e2b0e73d6c65
Jonatan Shlomov <jonatanshv@gmail.com>
2024-12-22 17:33:47 +0200

    service connected endpoints and repo

--- app/services/statistic_service.py
+++ app/services/statistic_service.py
@@ -1,19 +1,17 @@
-import base64
-import io
 import os
 import folium
-from matplotlib import pyplot as plt
 
 from app.db.mongo_database import db_url, db_name, collection_name
 from app.repository.mongo.attack_repository import AttackRepository
-from app.utils.graph_util import create_graph
+from app.utils.graph_util import create_graph_x_y
 
 
 def get_fatal_attack_types(top_num):
     mongo_repository = AttackRepository(db_url, db_name, collection_name)
     result = mongo_repository.get_all_fatal_attacks_by_group(top_num)
-
-    create_graph(result, "Attack Types", "Total Points", "Fatal Attacks by Group")
+    labels = [item["_id"] for item in result]
+    values = [item["total_points"] for item in result]
+    create_graph_x_y(labels, values, "Attack Types", "Total Points", "Fatal Attacks by Group")
 
     return result
 
@@ -38,7 +36,11 @@ def percentage_of_casualties_by_region(location_type, top_num):
 def get_top_five_attackers():
     mongo_repository = AttackRepository(db_url, db_name, collection_name)
     result = mongo_repository.top_five_attackers()
-    create_graph(result, "Group", "Fatalities", "Fatal Attacks by Attacker")
+    labels = [item["_id"] for item in result]
+    values = [item["sum"] for item in result]
+    create_graph_x_y(labels, values,  "Group", "Fatalities", "Fatal Attacks by Attacker")
     return result
 
+def get_geographical_attacks_hotspots():
+    pass
 
